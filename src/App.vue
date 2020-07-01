4b03a2273dc8c1b097dcbd462e495ddf7b0bf73f
harshaltrivedi10 <43525792+harshaltrivedi10@users.noreply.github.com>
2020-06-30 18:50:59 -0700

    Added Dark and Light mode toggler.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="app" class="app" :class="mode">
+    <Header :mode="mode" @toggle="toggle"></Header>
   </div>
 </template>
 
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  data() {
+    return {
+      mode: "light"
+    };
+  },
+  components: {
+    Header
+  },
+  methods: {
+    toggle() {
+      if (this.mode === "dark") {
+        this.mode = "light";
+      } else {
+        this.mode = "dark";
+      }
+    }
+  }
+};
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background: #f3f3f3;
+  color: #15202b;
+  transition: background 0.3s ease-out;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.dark {
+  background: #192734;
+  color: #f3f3f3;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="header">
+    <nav>
+      <div class="nav-link">One</div>
+      <div class="nav-link">Two</div>
+      <div class="nav-link">Three</div>
+      <div class="nav-link">Four</div>
+    </nav>
+    <Toggle :mode="mode" @toggle="$emit('toggle')"></Toggle>
+  </header>
+</template>
+
+<script>
+import Toggle from "@/components/Toggle.vue";
+export default {
+  props: ["mode"],
+  components: {
+    Toggle
+  }
+};
+</script>
+
+<style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 25px;
+  height: 60px;
+  align-items: center;
+  background: #c3c3c3;
+  transition: background 0.3s ease-out;
+}
+
+.header nav {
+  display: flex;
+  height: 60px;
+}
+
+.header nav .nav-link {
+  display: flex;
+  align-items: center;
+  padding: 0px 15px;
+}
+
+.dark header {
+  background: #15202b;
+}
+
+.header nav .nav-link:hover {
+  background: #b0b0b0;
+}
+
+.dark .header nav .nav-link:hover {
+  background: #101520;
+}
+</style>
